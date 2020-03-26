85d1398569ea6bfcaf1cd202ccb948df6d990b20
Daniel van Sleen <daniel_van_sleen@hotmail.com>
2020-03-26 19:03:13 +0100

    edit

--- server/server.js
+++ server/server.js
@@ -1,14 +1,1 @@
-
-const path = require('path');
-const express = require('express')
-
-const publicPath = path.join(__dirname, "/../public");
-const port = process.env.PORT || 3500;
-
-const app = express();
-
-app.use(express.static(publicPath));
-
-app.listen(3500, ()=>{
-    console.log(`Server is running: port ${port}`);
-})
+console.log('hello')
